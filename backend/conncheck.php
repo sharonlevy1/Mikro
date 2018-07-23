e2aa6cdce533fc4700d768beea0e35192db09869
yuliashoot <41567913+yuliashoot@users.noreply.github.com>
2018-07-23 16:45:02 +0300

    Add files via upload

---
+++ backend/conncheck.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "sharonsi_admin";
+$password = "mikro123456";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
